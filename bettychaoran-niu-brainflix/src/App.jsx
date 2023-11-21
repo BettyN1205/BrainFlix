676df33aac3d252a4c8f9df25a2afe631a409744
BettyNiu <bettychaoranniu@gmail.com>
2023-11-21 00:05:28 -0800

    header

---
+++ bettychaoran-niu-brainflix/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './components/Header';
+
+import './styles/globle.scss';
+import './styles/partials/header.scss';
+
+function App() {
+
+  return (
+    <>
+
+    <Header/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ bettychaoran-niu-brainflix/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import logo from "../assets/Logo/BrainFlix-logo.svg";
+import searchIcon from "../assets/Icons/search.svg";
+
+import uploadIcon from "../assets/Icons/upload.svg";
+
+import "../styles/partials/header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <img src={logo} alt="BrainFlix Logo" className="header__logo-img" />
+        </div>
+        <div className="header__search">
+          <img src={searchIcon} alt="searchIcon" className="header__search-icon" />
+          <input
+            type="text"
+            className="header__search-input"
+            placeholder="Search"
+          />
+        </div>
+        <div className="header__user">
+        </div>
+        <div className="header__upload">
+          <img src={uploadIcon} alt="uploadIcon" className="header__upload-icon" />
+          UPLOAD
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
