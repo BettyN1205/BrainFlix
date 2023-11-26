46ae59244b6d5234b17e0cd41eeb9822f2832233
BettyNiu <bettychaoranniu@gmail.com>
2023-11-25 17:11:31 -0800

    debug

--- bettychaoran-niu-brainflix/src/components/Hero/Hero.jsx
+++ bettychaoran-niu-brainflix/src/components/Hero/Hero.jsx
@@ -1,34 +1,38 @@
 import "../Hero/hero.scss";
-import eye from '../../assets/Icons/views.svg'
-import heart from '../../assets/Icons/likes.svg'
+import eye from "../../assets/Icons/views.svg";
+import heart from "../../assets/Icons/likes.svg";
 
-import {formatRelativeTime  } from "../timeFormat";
+import { formatRelativeTime } from "../timeFormat";
 
-const Hero = ({videoDetails}) => {
-
+const Hero = ({ videoDetails }) => {
   return (
     <>
       <section className="hero">
-  
-  <h1 className="hero__title">{videoDetails.title}</h1>
-  <div className="hero__details">
-    <div className="hero__info-container">
-    <div className="hero__info">
-      <span className="hero__info-name">By {videoDetails.channel}</span>
-      <span className="hero__info-date">{formatRelativeTime(videoDetails.timestamp)}</span>
-    </div>
-    <div className="hero__info">
-    <span className="hero__info-count"><img className="eyeIcon" src={eye} alt="eyeIcon" /> {videoDetails.views}</span>
-      <span className="hero__info-like"> <img className="heartIcon" src={heart} alt="heartIcon" /> {videoDetails.likes}</span>
-    </div>
-    </div>
-    
-    <p className="hero__description">
-      {videoDetails.description}
-    </p>
-  </div>
-</section>
+        <h1 className="hero__title">{videoDetails.title}</h1>
+        <div className="hero__details">
+          <div className="hero__info-container">
+            <div className="hero__info">
+              <span className="hero__info-name">By {videoDetails.channel}</span>
+              <span className="hero__info-date">
+                {formatRelativeTime(videoDetails.timestamp)}
+              </span>
+            </div>
+            <div className="hero__info">
+              <span className="hero__info-count">
+                <img className="eyeIcon" src={eye} alt="eyeIcon" />{" "}
+                {videoDetails.views}
+              </span>
+              <span className="hero__info-like">
+                {" "}
+                <img className="heartIcon" src={heart} alt="heartIcon" />{" "}
+                {videoDetails.likes}
+              </span>
+            </div>
+          </div>
 
+          <p className="hero__description">{videoDetails.description}</p>
+        </div>
+      </section>
     </>
   );
 };
